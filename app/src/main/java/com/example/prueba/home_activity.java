6a92005ff28bd199b68b74832cff180ab094d401
Lucero Macias <71266858+luceroMacias@users.noreply.github.com>
2020-10-29 00:57:28 -0600

    ACTUALIZACIÓN - PANTALLAS "COMPLETAS"
    Actualización

--- app/src/main/java/com/example/prueba/home_activity.java
+++ app/src/main/java/com/example/prueba/home_activity.java
@@ -1,6 +1,7 @@
 package com.example.prueba;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,9 +10,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.ExifInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.prueba.Fragments.FragmentConfiguracion;
 import com.example.prueba.Fragments.FragmentHistorial;
@@ -32,6 +41,7 @@ public class home_activity extends AppCompatActivity implements NavigationView.O
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +50,7 @@ public class home_activity extends AppCompatActivity implements NavigationView.O
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.navigationView);
+
 
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -101,10 +112,31 @@ public class home_activity extends AppCompatActivity implements NavigationView.O
             fragmentTransaction.commit();
         }
         if (menuItem.getItemId() == R.id.salir){
-
+            logout(this);
         }
 
 
         return false;
     }
+
+    private static void logout(final Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Salir");
+        builder.setMessage("¿Estás seguro que quieres salir?");
+        builder.setPositiveButton("SI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
 }
