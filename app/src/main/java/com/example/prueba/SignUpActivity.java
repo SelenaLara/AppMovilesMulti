202d0d4adaa46c7518609550f2b3c94d886a2ddf
Lucero Macias <71266858+luceroMacias@users.noreply.github.com>
2020-12-06 19:51:56 -0600

    Update
    BD implemetation

--- app/src/main/java/com/example/prueba/SignUpActivity.java
+++ app/src/main/java/com/example/prueba/SignUpActivity.java
@@ -10,11 +10,17 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -24,10 +30,53 @@ public class SignUpActivity extends AppCompatActivity {
     MaterialButton inicioSesion;
 
 
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    Button mButtonSubirDatosFirebase;
+    TextInputLayout mTextInputLayotDatoNombre,mTextInputLayotDatoEmail,mTextInputLayotDatoContraseña,mTextInputLayotConfirmaContraseña,mTextInputLayotNumeroDeCelular,mTextInputLayotDatoEdad,mTextInputLayotDatoSexo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mButtonSubirDatosFirebase= findViewById(R.id.registro);
+        mTextInputLayotDatoNombre= findViewById(R.id.nameTextField);
+        mTextInputLayotDatoEmail= findViewById(R.id.usuarioSignUpTextField);
+        mTextInputLayotDatoContraseña= findViewById(R.id.passTextField);
+        mTextInputLayotConfirmaContraseña= findViewById(R.id.passConfTextField);
+        mTextInputLayotNumeroDeCelular= findViewById(R.id.numeroTextField);
+        mTextInputLayotDatoEdad= findViewById(R.id.edadPacienteTextField);
+        mTextInputLayotDatoSexo= findViewById(R.id.sexTextField);
+
+        databaseReference=FirebaseDatabase.getInstance().getReference();
+
+        mButtonSubirDatosFirebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String nombre=mTextInputLayotDatoNombre.getEditText().getText().toString();
+                String Email=mTextInputLayotDatoEmail.getEditText().getText().toString();
+                String contraseña=mTextInputLayotDatoContraseña.getEditText().getText().toString();
+                String confirma_contraseña=mTextInputLayotConfirmaContraseña.getEditText().getText().toString();
+                String celular = mTextInputLayotNumeroDeCelular.getEditText().getText().toString();
+                int edad = Integer.parseInt(mTextInputLayotDatoEdad.getEditText().getText().toString());
+                String sexo=mTextInputLayotDatoSexo.getEditText().getText().toString();
+
+                //       InicializarFirebase();
+                CargarDatosFire(nombre, Email, contraseña, confirma_contraseña, celular, edad, sexo);
+
+            }
+//            private void InicializarFirebase () {
+//                FirebaseApp.initializeApp(this);FirebaseApp.initializeApp(this);
+//                FirebaseApp.initializeApp(this);
+//
+//                firebaseDatabase = FirebaseDatabase.getInstance();
+//                DatabasetReference=firebaseDatabase.getReference();
+//
+//            }
+
+        });
 
         signUpImageView = findViewById(R.id.signUpImageView);
         bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
@@ -52,6 +101,24 @@ public class SignUpActivity extends AppCompatActivity {
                 transitionBack();
             }
         });
+    }
+
+    private void CargarDatosFire(String nombre, String Email, String contraseña, String confirma_contraseña, String celular, int edad, String sexo) {
+        // InicializarFirebase();
+        Map<String, Object> DatosUsuario = new HashMap<>();
+        DatosUsuario.put("Nombre",nombre);
+        DatosUsuario.put("Email", Email);
+        DatosUsuario.put("Contraseña",contraseña);
+        DatosUsuario.put("ConfirmarContraseña",confirma_contraseña);
+        DatosUsuario.put("NumeroDeCelular",celular);
+        DatosUsuario.put("Edad",edad);
+        DatosUsuario.put("Sexo",sexo);
+
+
+        databaseReference.child ("Usuario").push().setValue(DatosUsuario);
+        //   DatabaseReference = firebaseDatabase.getReference (). child ("Usuario"); DatabaseReference.setValue ("ernesto");
+
+
     }
 
     @Override
